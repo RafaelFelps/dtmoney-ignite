4796f5f0980093b2cbefc7b08f5fdba2928afd7b
RafaelFelps <rafaelfelps62@gmail.com>
2021-04-04 13:30:53 -0300

    Adicionando o botao de Entrada e Saida

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,18 +1,19 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import fechar from "../../assets/fechar.svg";
 import incomeImage from '../../assets/entradas.svg';
 import outcomeImage from '../../assets/saidas.svg';
+import { useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({
-  isOpen,
-  onRequestClose,
-}: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -36,18 +37,24 @@ export function NewTransactionModal({
         <input type="number" placeholder="Valor" />
 
         <TransactionTypeContainer>
-          
-          <button
-            type="button">
+
+          <RadioBox
+            type="button"
+            onClick={() => { setType('deposit') }}
+            isActive={type === 'deposit'}
+          >
             <img src={incomeImage} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button
-            type="button">
-            <img src={outcomeImage} alt="Entrada" />
+          <RadioBox
+            type="button"
+            onClick={() => { setType('withdraw') }}
+            isActive={type === 'withdraw'}
+          >
+            <img src={outcomeImage} alt="Saida" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
 
         </TransactionTypeContainer>
 
