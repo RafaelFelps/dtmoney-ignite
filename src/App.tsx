6193e73b7b943710bb6ac070db99cc44d992f898
RafaelFelps <rafaelfelps62@gmail.com>
2021-04-17 14:25:40 -0300

    Adicionando contextos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -31,6 +32,6 @@ export function App() {
       ></NewTransactionModal>
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,41 +1,39 @@
 import { Container } from "./styles";
+import React, { useContext } from "react";
 
 import entradasImg from "../../assets/entradas.svg";
 import saidasImg from "../../assets/saidas.svg";
 import totalImg from "../../assets/total.svg";
+import {} from "../TransactionsTable";
+import { TransactionsContext } from "../../TransactionsContext";
 
-export function Summary(){
-    return(
-        <Container>
-            <div>
-                <header>
-                    <p>Entradas</p>
-                    <img src={entradasImg} alt="Entradas"/>
-                </header>
-                <strong>
-                    R$ 1000,00
-                </strong>
-            </div>
+export function Summary() {
+  const data = useContext(TransactionsContext);
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entradasImg} alt="Entradas" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </div>
 
-            <div>
-                <header>
-                    <p>Saidas</p>
-                    <img src={saidasImg} alt="Saidas"/>
-                </header>
-                <strong>
-                   - R$ 500,00
-                </strong>
-            </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={saidasImg} alt="Saidas" />
+        </header>
+        <strong>- R$ 500,00</strong>
+      </div>
 
-            <div className="highlight-background">
-                <header>
-                    <p>Total</p>
-                    <img src={totalImg} alt="Total"/>
-                </header>
-                <strong>
-                    R$ 500,00
-                </strong>
-            </div>
-        </Container>
-    );
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  );
 }
